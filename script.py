1c0d75f2892deb0731cc63ffd2ce029bb0f100d7
Alex Mandelias <alexmandelias@gmail.com>
2023-07-23 23:19:58 +0300

    Adds command line argument parsing skeleton
    Because there are multiple subcommands, subparsers will be used for each
    of them. The basic parser along with the parser for the show subcommand
    are implemented here. The rest shall be implemented following this
    pattern.

--- script.py
+++ script.py
@@ -1,3 +1,4 @@
+import argparse
 import json
 import praw
 import prawcore
@@ -9,8 +10,8 @@ def read_json(filename):
     return loaded_json
 
 # TODO: split main to different functions, one for each command
-# e.g. list/create/delete/add/...
-def main(feeds_filename, **kwargs):
+# e.g. show/create/delete/add/...
+def prototype_main(feeds_filename, **kwargs):
 
     # TODO: maybe dataclass or something for kwargs
     reddit = praw.Reddit(**kwargs)
@@ -46,9 +47,35 @@ def main(feeds_filename, **kwargs):
             # no 404 -> multireddit exists -> merge existing and file
             print(f"Feed {multireddit.display_name} already exists.")
 
+def _main_show(verbose, feeds):
+    print(f"Calling _main_show() with {verbose=} and {feeds=}")
+
+def main():
+
+    commands = {
+        "show": _main_show,
+    }
+
+    parser = argparse.ArgumentParser(description="Show existing feeds (wip). More commands to come!")
+    subparsers = parser.add_subparsers(title="subcommands", required=True, help="the command to execute")
+
+    curr_command = "show"
+    parser_show = subparsers.add_parser(curr_command, help="show existing feeds", formatter_class=argparse.RawTextHelpFormatter)
+    parser_show.add_argument("-v", "--verbose", default=2, type=int, choices=[i for i in range(0, 4)], help="how much information to show about each feed (default = %(default)s).\n"
+    "    0: just the name\n"
+    "    1: name and subreddit count\n"
+    "    2: name, subreddit count and names of subreddits\n"
+    "    3: name, subreddit count and names and descriptions of subreddits")
+    parser_show.add_argument("feeds", nargs="*", help="which feeds to show; defaults to all")
+    # TODO: more options like sorting alphabetically, by creation date, by subreddit count etc
+    parser_show.set_defaults(func=commands[curr_command])
+
+    # TODO: fix what happens on error (https://stackoverflow.com/questions/4042452/display-help-message-with-python-argparse-when-script-is-called-without-any-argu#answer-47440202)
+    args = parser.parse_args()
+    args_dict = vars(args)
+
+    func = args_dict.pop("func")
+    func(**args_dict)
+
 if __name__ == "__main__":
-    # TODO: remove hardcoded stuff and do some argparse instead
-    secret_file = "./secret/reddit_secrets.json"
-    feeds_file = "./feed_data/0feeds0.json"
-    secret = read_json(secret_file)
-    main(feeds_file, **secret)
+    main()
