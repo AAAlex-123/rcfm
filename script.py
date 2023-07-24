45eb437395382b172f67b741193b3feb3ce9d5ae
Alex Mandelias <alexmandelias@gmail.com>
2023-07-24 19:14:00 +0300

    Implements the basics of the 'show' command
    Currently support showing information about some or all of a Redditor's
    feeds with 4 levels of verbosity.

--- script.py
+++ script.py
@@ -50,6 +50,69 @@ def prototype_main(feeds_filename, **kwargs):
 def _main_show(reddit, username, verbose, feeds):
     print(f"Calling _main_show() with {reddit=}, {username=}, {verbose=} and {feeds=}")
 
+    if feeds == []:
+        multireddits = reddit.redditor(username).multireddits()
+        missing_names = []
+    else:
+        multireddits = []
+        missing_names = []
+        for multireddit_name in feeds:
+            multireddit = reddit.multireddit(redditor=username, name=multireddit_name)
+            if (_multireddit_exists(multireddit)):
+                multireddits.append(multireddit)
+            else:
+                missing_names.append(multireddit_name)
+
+    string = "'SHOW' STRING NOT SET TO ANYTHING"
+
+    if verbose == 0:
+        string = ', '.join(m.display_name for m in multireddits)
+
+    elif verbose == 1:
+        string = ""
+        for m in multireddits:
+            string += f"{m.display_name} ({len(m.subreddits)} subreddits), "
+
+        string = string[:-2]
+
+    elif verbose == 2:
+        string = ""
+        for m in multireddits:
+            subs = m.subreddits
+            string += f"{m.display_name} ({len(subs)} subreddits): "
+            string += f"{', '.join([sub.display_name for sub in subs])} \n"
+
+        string = string[:-2]
+
+    elif verbose == 3:
+        string = ""
+        for i, m in enumerate(multireddits):
+            subs = m.subreddits
+            string += f"""{f" {i+1}: {m.display_name} ({len(subs)} subreddits) ":-^80}\n"""
+            for j, subreddit in enumerate(subs):
+                string += f"--- {i+1}.{j+1}: {subreddit.display_name} ---\n{subreddit.description}\n\n"
+
+            string += "\n"
+
+    else:
+        raise ValueError(f"Unrecognized verbose level: {verbose}")
+
+    multireddit_count = len(multireddits)
+    if multireddit_count > 0:
+        print(f"Found {multireddit_count} feeds for user '{username}':")
+        print(string)
+
+    if len(missing_names) > 0:
+        print(f"The following feeds don't exist for user '{username}': {', '.join(missing_names)}")
+
+def _multireddit_exists(multireddit):
+    try:
+        multireddit.display_name
+    except prawcore.exceptions.NotFound as e:
+        return False
+    else:
+        return True
+
 def main():
 
     commands = {
